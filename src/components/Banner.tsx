468a73308ae94313f2b3e3cc5da59da927625be7
jawadiqbal962 <jawad.iqbal@xevensolutions.com>
2024-09-16 13:02:06 +0500

    Arrow button added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,19 @@
+// import { HiArrowCircleDown } from "react-icons/hi";
 import LeftBanner from "./LeftBanner";
 import RightBanner from "./RightBanner";
+// import { Link } from "react-scroll";
 
 const Banner = () => {
   return (
+    <>
     <section
       id="home"
       className="pointer-events-none w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-gray-700"
     >
-      
       <LeftBanner />
       <RightBanner />
     </section>
+      </>
   );
 };
 
